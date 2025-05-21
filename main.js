1d62ee8a514b6353be66b45a2290a6d7295746e7
rodrigo <140639687+piadonartio@users.noreply.github.com>
2025-05-21 14:27:02 -0300

    00

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const botoes = document.querySelectorAll(".botoes");
 const textos = document.querySelectorAll(".aba-conteudo");
-for (let i=0; i <botoes.length; i++){
-    botoes
-} [i].onclick = function(){
-    for (let j=0; j <botoes.length; j++){
-        botoes [j].classList.remove ("ativo");
-        textos [j].classList.remove ("ativo");
+for (let i=0; i < botoes.length; i++){
+    botoes [i].onclick = function(){
+        for (let j=0; j < botoes.length; j++){
+            botoes [j].classList.remove ("ativo");
+            textos [j].classList.remove ("ativo");
+        }
+        botoes [i].classList.add ("ativo");
+        textos [i].classList.add ("ativo");
     }
-    botoes [i].classList.add ("ativo");
-    textos [i].classList.add ("ativo");
-    
 }
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivol = new Date ("2026-01-01T00:00:00");
+const tempoObjetivo1 = new Date ("2026-01-01T00:00:00");
