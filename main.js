8d0944f56cadf874d8ac271b005814c182019106
rodrigo <140639687+piadonartio@users.noreply.github.com>
2025-06-04 13:58:01 -0300

    00

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const tempoObjetivo1 = new Date ("2026-01-01T00:00:00");
 const tempoObjetivo2 = new Date ("2027-08-23T19:00:00");
 const tempoObjetivo3 = new Date ("2029-10-22T20:00:00");
 const tempoObjetivo4 = new Date ("2030-04-18T06:00:00");
-const tempoObjetivo5 = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4, tempoObjetivo5]
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 function atualizarCronometro (){
     for (let i=0; i < contadores.length; i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos [i])[0];
