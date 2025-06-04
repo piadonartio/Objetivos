45440834ac8933580a65b7e4e013be05ddd9b396
rodrigo <140639687+piadonartio@users.noreply.github.com>
2025-06-04 14:09:43 -0300

    000

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 function atualizarCronometro (){
     for (let i=0; i < contadores.length; i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos [i])[0];
-         document.getElementById("horas"+i).textContent = calculaTempo(tempos [i])[1];
-          document.getElementById("min"+i).textContent = calculaTempo(tempos [i])[2];
-           document.getElementById("seg"+i).textContent = calculaTempo(tempos [i])[3];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos [i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos [i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos [i])[3];
     }
     for (let i = 0; i < contadores.length; i++){
         //contadores[i].textContent = calculaTempo (tempos [i]);
